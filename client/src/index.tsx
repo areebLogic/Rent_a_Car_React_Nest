b7ea8a82d25b2c0bc62286c3b6e967d7b3de6901
areebLogic <areebLogic@users.noreply.github.com>
2021-11-30 06:33:19 +0500

    No message

---
+++ client/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from './services/graphql';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store} >
+      <ApolloProvider client={apolloClient} >
+        <App />
+      </ApolloProvider>
+    </Provider>
+  </React.StrictMode>
+);
